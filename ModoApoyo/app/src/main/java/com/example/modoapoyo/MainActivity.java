13f85c4265761426a5425b39decf64eeed2bf20b
dmanjarrez15 <diegomanjarrez1508@gmail.com>
2026-06-05 08:01:46 -0600

    beta 2

--- ModoApoyo/app/src/main/java/com/example/modoapoyo/MainActivity.java
+++ ModoApoyo/app/src/main/java/com/example/modoapoyo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.modoapoyo;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +9,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txtResultado=findViewById(R.id.txtResultado);
+
+        String url="http://10.0.2.2:8080/usuarios";
+
+        RequestQueue queue=Volley.newRequestQueue(this);
+        JsonArrayRequest request = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                response ->
+                        txtResultado.setText(response.toString()),
+                error ->
+                        txtResultado.setText(error.toString())
+        );
+        queue.add(request);
     }
 }
